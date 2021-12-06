9a5a4d5bff67d3b6f7359640fca5ef95438698e6
maxtwoone <maxkreienbaum@gmail.com>
2021-12-06 15:15:01 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///HMS.db'
+db = SQLAlchemy(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+# TODO: change debug mode
